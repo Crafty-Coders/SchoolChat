bd507f8c6fb444182197975bda39415efc85fa85
= <=>
2021-11-22 22:44:25 +0300

    upd

--- WS/ws.js
+++ WS/ws.js
@@ -1,9 +1,27 @@
 const { OK, Chat } = require("../DB/DB_init");
 const { MessageDB, ChatUserDB } = require("../DB/DB_main");
-
 const io = require("socket.io")(mobile_server);
+
+async function get_users_online() {
+    let online = []
+    for (var i in io.sockets)
+        online.push(io.sockets[i].user_id)
+    return online
+}
+
 io.on('connection', (socket) => {
     console.log("new")
+
+    // ! socket ~ User (AuthDB)
+    // ? Getting req_data when auth(POST-GET request)
+    socket.user_id = socket.handshake.query.user_id
+    socket.name = socket.handshake.query.name
+    socket.surname = socket.handshake.query.surname
+    socket.school_id = socket.handshake.query.school_id
+    socket.class_id = socket.handshake.query.class_id
+    socket.phone = socket.handshake.query.phone
+    socket.email = socket.handshake.query.email
+    socket.picture_url = socket.handshake.query.picture_url
 
     socket.on('chat-msg', (data) => {
         MessageDB.new_msg(data).then(res => {
@@ -51,20 +69,27 @@ io.on('connection', (socket) => {
     })
 
     socket.on('get-info', (data) => {
-        switch(data.flag){
+        switch (data.flag) {
             case "chat":
-                ChatUserDB.get_user_info(data.data).then(res => 
+                ChatUserDB.get_user_info(data.data).then(res =>
                     socket.emit('chat_info', res)).catch(err =>
-                    socket.enit('chat_info', err))
+                        socket.enit('chat_info', err))
                 break
             case "user":
-                ChatUserDB.get_user_info(data.data).then(res => 
+                ChatUserDB.get_user_info(data.data).then(res =>
                     socket.emit('user_info', res)).catch(err =>
-                    socket.enit('user_info', err))
+                        socket.enit('user_info', err))
                 break
         }
     })
 
-    
+    socket.on('get-online', (data) => {
+        get_users_online().then(res => 
+            socket.emit('online-incoming', res)).catch(err => 
+                socket.emit('online-input', ERR))
+    })
+
+
+
 })
 module.exports = { io }
--- WS/ws_test.js
+++ WS/ws_test.js
@@ -1,6 +1,13 @@
+const { query } = require('express')
 const io = require('socket.io-client')
-var socket = io.connect('http://localhost:3000')
-socket.emit("chat-msg", {a: 5, b: 7})
+let User1_data = {
+
+}
+var User1 = io.connect('http://localhost:3000', {query: User1_data})
+let User2_data = {
+
+}
+var User2 = io.connect('http://localhost:3000', {query: User2_data})
 
 socket.on('stat', (res) => {
     console.log(res)
