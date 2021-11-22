77a9fe0c750ae59baef83ee7a5cb08165d785bc8
lalkalol1907 <kostyaleonov13@yandex.ru>
2021-11-22 08:43:10 +0300

    upd

--- WS/ws.js
+++ WS/ws.js
@@ -1,11 +1,12 @@
 const io = require("socket.io")(mobile_server);
 io.on('connection', (socket) => {
-    console.log("new")
+    console.log("new", socket)
     socket.username = "aboba"
-    
+
     socket.on('change', (data) => {
         console.log(data)
         socket.username = data.username
+        socket.emit('stat', "OK")
     })
 })
 module.exports = {io}
--- WS/ws_test.js
+++ WS/ws_test.js
@@ -1,3 +1,8 @@
+const { reset } = require('chalk')
 const io = require('socket.io-client')
 var socket = io.connect('http://localhost:3000')
 socket.emit("change", {a: 5, b: 7})
+
+socket.on('stat', (res) => {
+    console.log(res)
+})
