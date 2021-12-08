b2d7927f37df18fa152bbef9d402cd119a6a4c62
lalkalol1907 <kostyaleonov13@yandex.ru>
2021-12-08 23:35:21 +0300

    upd

--- DB/Tables/Chat-User.js
+++ DB/Tables/Chat-User.js
@@ -5,12 +5,14 @@ async function get_user_chats(data) {
     /**
      * data = {user_id}
      */
-    if (!data_checker(data, ["user_id"]))
+    if (!data_checker(data, ["user_id"])) {
         console.log("data checker")
         return DATA;
-    if (!(await check_exist({ id: data.user_id }, "user")))
+    }
+    if (!(await check_exist({ id: data.user_id }, "user"))) {
         console.log("not_exist")
         return DATA;
+    }
 
     let chats = await ChatUser.findAll({
         attributes: ['chat_id'],
