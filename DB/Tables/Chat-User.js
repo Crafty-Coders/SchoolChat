277a80527112ef22653b09ea92025a467a8c6873
lalkalol1907 <kostyaleonov13@yandex.ru>
2022-01-25 14:58:49 +0300

    upd

--- DB/Tables/Chat-User.js
+++ DB/Tables/Chat-User.js
@@ -135,7 +135,7 @@ async function get_chat_info(data) {
         dat = {
             id: chat.id,
             users: [...new Set(users)],
-            name: `${chat.name}`,
+            name: utf8.encode(chat.name),
             admins: admins,
             pic: chat.picture_url,
             time: chat.createdAt,
