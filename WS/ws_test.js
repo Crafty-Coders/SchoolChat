c1970e72e12853bace71b4c57dec0678ee1379dd
lalkalol1907 <kostyaleonov13@yandex.ru>
2022-01-27 01:47:46 +0300

    clean

--- WS/ws_test.js
+++ WS/ws_test.js
@@ -1,31 +1,31 @@
-// const { query } = require('express')
-// const io = require('socket.io-client')
-// const chalk = require('chalk')
-// const { findLastKey } = require('lodash')
-// let User1_data = {
-//     user_id: 1,
-//     name: 1,
-//     surname: 1,
-//     school_id: 1,
-//     class_id: 1,
-//     phone: 1,
-//     email: 7
-// }
-// var User1 = io.connect('http://localhost:3000', { query: User1_data })
-// /*let User2_data = {
-//     user_id: 2
-//     name: 2,
-//     surname: 2,
-//     school_id: 2,
-//     class_id: 2,
-//     phone: 2,
-//     email: 2
-// }
-// var User2 = io.connect('http://localhost:3000', {query: User2_data})*/
+const { query } = require('express')
+const io = require('socket.io-client')
+const chalk = require('chalk')
+const { findLastKey } = require('lodash')
+let User1_data = {
+    user_id: 1,
+    name: 1,
+    surname: 1,
+    school_id: 1,
+    class_id: 1,
+    phone: 1,
+    email: 7
+}
+var User1 = io.connect('ws://school-chat-server-ws.herokuapp.com/', { query: User1_data })
+/*let User2_data = {
+    user_id: 2
+    name: 2,
+    surname: 2,
+    school_id: 2,
+    class_id: 2,
+    phone: 2,
+    email: 2
+}
+var User2 = io.connect('http://localhost:3000', {query: User2_data})*/
 
-// User1.emit("newMessage", { 'chat_id': 2, 'user_id': 1, 'text': 'Я оптимизирую код ', 'attachments': {}, 'deleted_user': false, 'deleted_all': false, 'edited': false })
-// User1.on('chat-message-recieve', (data => console.log(data)))
-const auth = require('../DB/Tables/Auth')
-auth.change_name_surname({"id": 1, "name": "Mihail", "surname": "Frolov"}).then(res => {
+User1.emit("newMessage", { 'chat_id': 1, 'user_id': 2, 'text': 'Я оптимизирую код ', 'attachments': {}, 'deleted_user': false, 'deleted_all': false, 'edited': false })
+User1.on('chat-message-recieve', (data => console.log(data)))
+// const auth = require('../DB/Tables/Auth')
+// auth.change_name_surname({"id": 1, "name": "Mihail", "surname": "Frolov"}).then(res => {
 
-})
+// })
